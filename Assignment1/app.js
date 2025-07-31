b6fb80869590733c19ac92c289dc1848eb19f8f9
Unshakhan <unshak08@gmail.com>
2025-07-31 17:59:42 +0500

    Update files

---
+++ Assignment1/app.js
@@ -0,0 +1,35 @@
+// __________Ch1_____________
+// __________Q1_______________
+//alert("Welcome!!!")
+//_________Q2__________
+//alert("Error! Please enter a vald password.")
+//_________Q3__________
+//alert(" Welcome to JS Land...\n Happy Coding")
+//_________Q4__________
+// alert(" Welcome to JS Land...")
+// alert(" Happy Coding \n Prevent this page from creating additional dialogts")
+//_________Q5__________
+//console.log("Hello... I can run JS through my web browser's console");
+// ________________________________________________________________Ch2_______________________________
+// __________Q1_______________
+// var userName = ""
+// // __________Q2_______________
+// var myName = "Unsha"
+// // __________Q3_______________
+// var message = '"Hello World"'
+// alert(message)
+// // __________Q4_______________
+// var studentName = '"John Doe"'
+// alert(studentName)
+// var age ="15 years old"
+// alert(age)
+// var certificate ="Certified Mobile Application Development"
+// alert(certificate)
+// __________Q5_______________
+//alert(" PIZZA \n PIZZ \n PIZ \n PI \n P")
+// __________Q6_______________
+// var email = "example@example.com"
+// alert("My email is " + email)
+//__________Q7_______________
+var book = "A smarter way to learn JavaScript"
+alert("My email is " + email)
