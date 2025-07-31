7bc1254b0de6690d9cb97827da25653c5928c3d2
Unshakhan <unshak08@gmail.com>
2025-07-31 22:23:27 +0500

    Update files

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -31,5 +31,78 @@
 // var email = "example@example.com"
 // alert("My email is " + email)
 //__________Q7_______________
-var book = "A smarter way to learn JavaScript"
-alert("My email is " + email)
+// var book = " A smarter \n way to learn JavaScript"
+// alert("I am trying to learn from the book " + book)
+//___________Q8___________
+//document.write("YAH! I can write HTML content through Javascript");
+//________Q9________
+// var uniqueString = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(uniqueString)
+//  let greeting = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// document.body.innerHTML = greeting;
+//_____________________________________________________Ch3______________________________________________
+//_________Q1_____________
+// var age ="I am 15 years old"
+// alert(age)
+//_________Q2_____________
+// Get current visit count from localStorage
+// let visitCount = localStorage.getItem('visitCount');
+
+// // Check and update the count
+// if (visitCount === null) {
+//   visitCount = 1;
+// } else {
+//   visitCount = parseInt(visitCount) + 1;
+// }
+
+// // Save it back
+// localStorage.setItem('visitCount', visitCount);
+
+// // Show it in a popup
+// alert(`You have visited this site ${visitCount} times.`);
+//_________Q3_____________
+//  let birthYear = "My birth year is 1990\nData type of my birth declared variable is number";
+//  document.body.innerHTML = birthYear;
+//___________Q4___________--
+//  let visitorName = "John Doe";
+// let productTitle = "T-shirt(s)";
+// let quantity = 5;
+// let message = "<b>" + visitorName + "</b> ordered <b>" + quantity + " " + productTitle + "</b> on XYZ Clothing store.";
+// document.body.innerHTML = message;
+//__________________________________________ch4___________________________________-
+//_____________Q1___________
+// var name = "Unsha", age = 20, city = "Karachi"
+//___________Q2______________
+// 5 Legal  Variable name
+// let userName;
+// let $amount;
+// let _score;
+// let age1;
+// let totalMarks;
+// 5 illegal  Variable name
+// let 1name;        
+// let user name;    
+// let var;          
+// let my-name;      
+// let @score; 
+//___________Q3______________
+// let heading = document.createElement("h1");
+// heading.textContent = "Rules for naming JS variables";
+
+// let lineBreak = document.createElement("br"); // line break
+
+// let paragraph = document.createElement("p");
+// paragraph.textContent = " 1) Variable names can only contain alphabets, numbers,underscore and dollar.For example $my_1stVariable";
+// let paragraph2 = document.createElement("p");
+// paragraph2.textContent = "2) hellVariables must begin with a small letters, underscore or dollar. For example $name, _name or name"
+// let paragraph3 = document.createElement("p");
+// paragraph3.textContent =" 3) Variable names are case sensitive"
+// let paragraph4 = document.createElement("p");
+// paragraph4.textContent ="4) Variable names should not be JS keywords"
+
+// document.body.appendChild(heading);
+// document.body.appendChild(lineBreak);  
+// document.body.appendChild(paragraph);
+// document.body.appendChild(paragraph2);
+// document.body.appendChild(paragraph3);
+// document.body.appendChild(paragraph4);
