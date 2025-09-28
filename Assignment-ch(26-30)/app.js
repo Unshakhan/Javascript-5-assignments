51af5b3cc5565fd8f395a0fbdb4ad142d11df983
Unshakhan <unshak08@gmail.com>
2025-09-28 17:38:18 +0500

    Update files

---
+++ Assignment-ch(26-30)/app.js
@@ -0,0 +1,62 @@
+//_____________Q1
+// var int = Number(prompt("Enter any positive integer"))
+// document.write(int + "<br>")
+// document.write("Round of value of the Number: " + Math.round(int) + "<br>")
+// document.write("Floor value of the Number: " + Math.floor(int) + "<br>")
+// document.write("Ceil value of the Number: " + Math.ceil(int)+ "<br>")
+//_____________Q2
+
+// var int = Number(prompt("Enter any negative floating point"))
+// document.write(int + "<br>")
+// document.write("Round of value of the Number: " + Math.round(int) + "<br>")
+// document.write("Floor value of the Number: " + Math.floor(int) + "<br>")
+// document.write("Ceil value of the Number: " + Math.ceil(int))
+//_____________Q3
+
+// var num = +prompt("Enter a number:")
+// console.log(Math.abs(num));
+//____________OR
+// let number = -4;
+// let absoluteValue = Math.abs(number);
+// console.log("The absolute value of " + number + " is " + absoluteValue);
+//_____________Q4
+// var dicenum = Math.floor(Math.random()*6 + 1)
+// document.write("Random dice value is : " + dicenum)
+//_____________Q5
+
+// var dicenum = Math.round(Math.random() + 1)
+// // console.log(dicenum);
+// if(dicenum===1){
+//     console.log(dicenum + "\nrandom coin value : Tails");
+// }else{
+//     console.log(dicenum+"\nrandom coin value : Heads");
+// }
+//_____________Q6
+// var dicenum = Math.floor(Math.random()*100 + 1)
+// console.log("Random num between 1 and 100 : " + dicenum);
+//_____________Q7
+// var UserWeight = prompt("Enter your weight in kilogram(Kg)")
+// var weight = parseFloat(UserWeight)
+// var unit = "";
+// if (UserWeight.toLowerCase().includes("kg") || UserWeight.toLowerCase().includes("kilograms") ) {
+//   unit = "kg";
+//   console.log("The weight of the user is : " + weight + unit);
+// }else{
+//     console.log( "The weight of the user is : " + weight);
+    
+// }
+//_____________Q8
+// var secret = Math.floor(Math.random() * 10) + 1;
+// var userSecretNum = Number(prompt("Enter secret number from 1 to 10"));
+// var username = prompt("Enter your name");
+
+// if (!userSecretNum || userSecretNum < 1 || userSecretNum > 10) {
+//   console.log("Please enter a valid number between 1 and 10, " + username);
+// } else if (secret === userSecretNum) {
+//   console.log("Congratulations " + username + "! You guessed the secret number!");
+// } else {
+//   console.log("Try again, " + username + ". The secret number was " + secret);
+// }
+//_____________Q9
+
+
---
+++ Assignment-ch(35-39)/app.js
@@ -0,0 +1,107 @@
+
+
+// function currentDate(){
+//     console.log(new Date());
+// }
+// currentDate()
+//______________Q2
+// function userFulName (num1,num2){
+//  console.log("Welcome to SMIT ! " + num1 + " " + num2);
+// }
+// var FirstName = prompt("Enter your First Name")
+// var LastName = prompt("Enter your Last Name")
+// userFulName (  FirstName , LastName)
+//______________Q3
+
+//  function sum (num1,num2){
+//  console.log(num1 + num2);
+// }
+// var numa = +prompt("Enter your First num")
+// var numb = +prompt("Enter your Last num")
+// sum ( numa,numb)
+// ______________Q4
+// function userInput(num1, num2, operator) {
+//     if (operator === "+") {
+//         var sum = num1 + num2
+//     }
+//     else if (operator === "-") {
+//         var sum = num1 - num2
+//     }
+//     else if (operator === "/") {
+//         var sum = num1 / num2
+//     }
+//     else if (operator === "*") {
+//         var sum = num1 * num2
+//     }
+//     else if (operator === "%") {
+//         var sum = num1 % num2
+        
+//     } else {
+//         sum ="Invalid Operation!";
+//     }
+//     return sum
+// }
+// var userFirstNum = Number(prompt("Enter your first num"))
+// var userLastNum = Number(prompt("Enter your Last num"))
+// var UserOperator = prompt("Enter an Operator")
+
+// console.log(userInput(userFirstNum, userLastNum, UserOperator));
+// ______________Q5
+// function calculateSquare(x){
+//     // return x*x
+//     return x**2
+// }
+// console.log(calculateSquare(6));
+
+// ______________Q6
+// function factorial(n){
+//     var result = 1
+//     for(var i=1;i<=n;i++){
+//         result *=i
+//     }
+//    return result
+// }
+// document.write(factorial(Number(prompt("Enter a number"))))
+// ______________Q7
+
+// function factorial(num1,num2){
+//        result = ""
+//         for(num1;num1<=num2;num1++){
+//             result += num1
+//         }
+//         console.log(result + "<br>");
+//        return result
+//     }
+//     var firstNum = Number(prompt("Enter a Number"))
+//     var SecNum = Number(prompt("Enter a Sec Number"))
+// factorial(firstNum,SecNum);
+//______________OR
+// function factorial(a,b){
+//     for(i=a; i<=b;i++){
+//         console.log(i);
+//     }
+
+// }
+//     factorial(1,20)
+
+// ______________Q8
+
+// function calculateHyp(base,perp){
+//     function calculateSquare(x){
+//             // return x*x
+//             return x**2
+//         } 
+//         var Hyp= calculateSquare(base)+calculateSquare(perp)
+// return Hyp
+// }
+// console.log(calculateHyp(10,5));
+
+// ______________Q9
+// function calculateArea(w,h){
+//     return w*h
+// }
+// console.log(calculateArea(10,5));//_______oR
+// var width = 30
+// height = 10
+// console.log(calculateArea(width,height));
+//_______________Q10
